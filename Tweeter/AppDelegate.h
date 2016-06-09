76b27cc7c1ea8f3cce4dd7d73f16b7959fbd746e
Claudia Toh Yi An <Claudia@claudias-macbook-pro-2.local>
2016-06-09 17:31:16 +0800

    Initial Commit

---
+++ Tweeter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tweeter
+//
+//  Created by Claudia Toh Yi An on 9/6/16.
+//  Copyright © 2016 Claudia Toh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tweeter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tweeter
+//
+//  Created by Claudia Toh Yi An on 9/6/16.
+//  Copyright © 2016 Claudia Toh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

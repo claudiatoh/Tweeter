793b93debaca88d48a18fd1d00e0b80df63ad28b
Claudia <claudiatohyian@hotmail.com>
2016-07-12 14:35:51 +0800

    Completed functions.

--- Tweeter/ViewController.m
+++ Tweeter/ViewController.m
@@ -9,7 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *twitterInputText;
 
+- (void) configureTweetTextView;
 @end
 
 @implementation ViewController
@@ -17,11 +19,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self configureTweetTextView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)showShareAction:(id)sender {
+}
+
+- (void) configureTweetTextView{
+    self.twitterInputText.layer.backgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:0.9 alpha:1.0].CGColor;
+    self.twitterInputText.layer.cornerRadius=10.0;
+    self.twitterInputText.layer.borderColor= [UIColor colorWithWhite:0 alpha:0.5].CGColor;
+    self.twitterInputText.layer.borderWidth=2.0;
+    
 }
 
 @end
